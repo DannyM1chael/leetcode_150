217366c7f7f37f1d143cffe29f7f857e9ab79103
Denis Mikhalev <denis.mikhalev.pro@gmail.com>
2025-09-16 10:58:43 +0200

    reverse words in a string

---
+++ array_string/reverse_words_in_a_string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    const words = s.split(' ').filter(Boolean)
+    let left = 0
+    let right = words.length - 1
+
+    while (left < right) {
+        [words[left], words[right]] = [words[right], words[left]]
+        left++
+        right--
+    }
+
+    return words.join(' ')
+};
+
+console.log(reverseWords("the sky is blue"));
