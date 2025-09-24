d067da95bc4cba5b1c313c04816b4519cf33d6bd
Denis Mikhalev <denis.mikhalev.pro@gmail.com>
2025-09-24 10:39:42 +0200

    implement space efficient of compare version numbers

--- two_pointers/compare_version_numbers.js
+++ two_pointers/compare_version_numbers.js
@@ -24,6 +24,44 @@ var compareVersion = function(version1, version2) {
     return 0;
 };
 
+var compareVersionSpaceEfficient = function(version1, version2) {
+    function getNextNumber(version, startPos) {
+        if (startPos >= version.length) return { number: 0, nextPos: startPos };
+        let numString = '';
+        let i = startPos;
+
+        while (i < version.length && version[i] !== '.') {
+            numString += version[i];
+            i++;
+        }
+
+        const number = parseInt(numString);
+        const nextPos = version[i] === '.' ? i + 1 : i;
+
+        return { number, nextPos };
+    }
+
+    let pos1 = 0
+    let pos2 = 0
+
+    while (pos1 < version1.length || pos2 < version2.length) {
+        const num1 = getNextNumber(version1, pos1)
+        const num2 = getNextNumber(version2, pos2)
+
+        if (num1.number > num2.number) return 1;
+        if (num1.number < num2.number) return -1;
+
+        pos1 = num1.nextPos
+        pos2 = num2.nextPos
+    }
+
+    return 0;
+}
+
 console.log(compareVersion("1.2", "1.10"));
 console.log(compareVersion("1.01", "1.001"));
 console.log(compareVersion("1.0", "1.0.0.0"));
+
+console.log(compareVersionSpaceEfficient("1.2", "1.10"));
+console.log(compareVersionSpaceEfficient("1.01", "1.001"));
+console.log(compareVersionSpaceEfficient("1.0", "1.0.0.0"));
