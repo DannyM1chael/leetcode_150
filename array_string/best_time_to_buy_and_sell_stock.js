d55b4cc58f69a8a9194e45d9b3897eb84ea3b9c4
Denis Mikhalev <denis.mikhalev.pro@gmail.com>
2025-09-26 14:52:55 +0200

    best time to buy and sell stocks

---
+++ array_string/best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices) => {
+    let minPrice = prices[0]
+    let maxProfit = 0
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) minPrice = prices[i]
+        if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice
+    }
+
+    return maxProfit
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
